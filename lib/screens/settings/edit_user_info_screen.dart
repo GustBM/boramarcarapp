79ccc7da23b69880fa1a782627c571839f6525b6
Gustavo Barros <gustbm78@gmail.com>
2021-09-06 17:14:40 -0300

    Ajuste dos imports

--- lib/screens/settings/edit_user_info_screen.dart
+++ lib/screens/settings/edit_user_info_screen.dart
@@ -1,17 +1,17 @@
 import 'dart:io';
 
-import 'package:boramarcarapp/models/user.dart';
-import 'package:boramarcarapp/providers/users.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_form_builder/flutter_form_builder.dart';
-
-import 'package:boramarcarapp/widgets/app_drawer.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:mask_text_input_formatter/mask_text_input_formatter.dart';
 import 'package:provider/provider.dart';
+
+import 'package:boramarcarapp/widgets/app_drawer.dart';
+import 'package:boramarcarapp/models/user.dart';
+import 'package:boramarcarapp/providers/users.dart';
 
 import '../../utils.dart';
 
